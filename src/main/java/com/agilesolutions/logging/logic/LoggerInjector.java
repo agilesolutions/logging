949307a795547eca3008701d0833a3046ce6a5e0
Robert Rong <robert.rong@agile-solutions.ch>
2020-02-29 16:45:27 +0100

    f

--- src/main/java/com/agilesolutions/logging/logic/LoggerInjector.java
+++ src/main/java/com/agilesolutions/logging/logic/LoggerInjector.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
+@Component
 public class LoggerInjector implements BeanPostProcessor {
 
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
